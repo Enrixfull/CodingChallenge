4ceb926d7aba614a662b8e314c8bc6005ee54e96
Omar Enrique Ramos Gonzalez <ramos.g.omar@gmail.com>
2017-12-02 20:19:19 -0600

    First Commit, UI completed, Use of tab menu for navigation

---
+++ CodingChallenge/Controller/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  CodingChallenge
+//
+//  Created by Omar Ramos González on 02/12/17.
+//  Copyright © 2017 Omar Ramos González. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
